bac41d9c15d12549cd8a63d912a774143b5949b3
sasakifuruta <rixiaozig@gmail.com>
2025-07-12 18:47:41 +0900

    Fix flake8 errors

--- app/main.py
+++ app/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 import os
 
+
 app = FastAPI()
 
 if os.getenv("ENV") != "production":
     load_dotenv()  # .env を読み込む
+
 
 @app.get("/")
 def read_root():
